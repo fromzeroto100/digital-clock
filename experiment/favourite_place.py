d82ddf162b69e0bd44a17be99bfb641f85d4199d
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2023-02-19 13:19:23 +0100

    1

---
+++ experiment/favourite_place.py
@@ -0,0 +1,7 @@
+favourite_places = {
+    'Alex': 'Hawaian beach',
+    'David': 'London bridge',
+    'Ali': 'Dubai city'
+}
+for name, place in favourite_places.items():
+    print(name.title(), place.title())
