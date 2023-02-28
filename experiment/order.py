680053c7dcef72a9e0acdb3213eb797dff58b6a3
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2023-02-28 21:58:17 +0100

    2

---
+++ experiment/order.py
@@ -0,0 +1,5 @@
+def make_order(size, text):
+    print("\nMy size is " + size + ".")
+    print(text + " must be written.")
+
+make_order('L', 'I love wrestling')    
--- experiment/parrot.py
+++ experiment/parrot.py
@@ -1,6 +1,7 @@
 prompt = '\nTell me something and I will repeat it to you: '
 prompt += '\nPress "quit" to finish the program'
 
+message = ''
 while message != "quit":
     message = input(prompt)
     print(message)
